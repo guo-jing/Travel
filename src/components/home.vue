6db97a82128a9c9c0e3852d9192265b42b9b90bf
guojing <343630774@qq.com>
2020-09-20 20:29:41 +0800

    init

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+    hello world
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  name: 'name'
 }
 </script>
 
-<style>
+<style scoped>
 
 </style>
